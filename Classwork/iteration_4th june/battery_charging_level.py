328a08f566185583b970882456c5c6542626c3de
Tushar Rajput <a2024cseaiml10238@imsec.ac.in>
2026-06-04 11:01:42 +0530

    Program to displaying battery charging level
    Implement battery charging simulation with a loop.

---
+++ Classwork/iteration_4th june/battery_charging_level.py
@@ -0,0 +1,9 @@
+charging_level = 20
+electricity_status = True
+while(charging_level <=100):
+    if(electricity_status):
+         print("Battery level:" ,charging_level,"%")
+         charging_level= charging_level + 10
+    else:
+         break
+print("Full Charge") 
