913a73ea88cd8c2b8c202361752c3421387ea036
Tushar Rajput <a2024cseaiml10238@imsec.ac.in>
2026-06-04 12:19:40 +0530

    Program to check entered user marks is 40 or more

---
+++ Classwork/iteration_4th june/Online_exam_portal.py
@@ -0,0 +1,8 @@
+marks = int(input("Enter Marks: "))
+
+while marks < 40:
+    print("Result: Fail")
+    marks = int(input("Enter Marks: "))
+
+print("Result: Pass")
+print("Congratulations! You have cleared the assessment.")
