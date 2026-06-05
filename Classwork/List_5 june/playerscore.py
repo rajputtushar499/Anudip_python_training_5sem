dd4d8cc0d19fae5289f03b0d914ef18840cf82f0
Tushar Rajput <a2024cseaiml10238@imsec.ac.in>
2026-06-05 11:56:29 +0530

    Program to calculate highest score of 11 player

---
+++ Classwork/List_5 june/playerscore.py
@@ -0,0 +1,12 @@
+# PLAYER SCORE
+player_score = []
+# input of score form user
+for i in range(11):
+    score = int(input("Enter the score of player {}: "))
+    player_score.append(score)
+# display the score of player
+print("\n---- Player Scores -----")
+print ("score of 11 players: ", player_score)
+#finding the highest score
+highest_score = max(player_score)
+print("Highest score is:", highest_score)
