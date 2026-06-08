41208c38768a1b5ecaff2bf6d841f4f9221e706c
Tushar Rajput <a2024cseaiml10238@imsec.ac.in>
2026-06-08 15:45:04 +0530

    the frequency of vowels which are present in given sentence

--- Classwork/string_8 june/vowels.py
+++ Classwork/string_8 june/vowels.py
@@ -1,7 +1,32 @@
 #write a program to find out input a sentence and display the frequency of vowels which are present in given sentence ignoring the case of the characters
+
 sentence = input("Enter a sentence: ")
-vowel_count = 0
-for char in sentence:
-    if char in "aeiou":
-        vowel_count += 1
-print("Frequency of vowels in the sentence:", vowel_count)
+
+a = e = i = o = u = 0
+
+# Convert sentence to lowercase and check each character
+for ch in sentence.lower():
+    if ch == 'a':
+        a += 1
+    elif ch == 'e':
+        e += 1
+    elif ch == 'i':
+        i += 1
+    elif ch == 'o':
+        o += 1
+    elif ch == 'u':
+        u += 1
+        
+# Display only those vowels whose frequency is greater than 0
+print("Frequency of vowels:")
+
+if a > 0:
+    print("A =", a)
+if e > 0:
+    print("E =", e)
+if i > 0:
+    print("I =", i)
+if o > 0:
+    print("O =", o)
+if u > 0:
+    print("U =", u)
